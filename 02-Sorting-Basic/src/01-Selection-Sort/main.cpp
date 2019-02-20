b94843144e8f6b3e0ff9d8d426860f409701eb43
ttom921 <ttom921@hotmail.com>
2019-02-20 14:52:44 +0800

    加入基本算法

---
+++ 02-Sorting-Basic/src/01-Selection-Sort/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void selectionSort(int arr[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		//尋找[i,n)區間里的最小值
+		int minIndex = i;
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[minIndex])
+				minIndex = j;
+		}
+		swap(arr[i], arr[minIndex]);
+	}
+}
+
+int main()
+{
+	int a[10] = { 10,9,8,7,6,5,4,3,2,1 };
+	selectionSort(a, 10);
+	for (int i = 0; i < 10; i++)
+	{
+		cout << a[i] << " ";
+	}
+	cout << endl;
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Play-with-Algorithms
 看慕課網的算法與數據結構
+[參考的github](https://github.com/liuyubobobo/Play-with-Algorithms)
+
+
